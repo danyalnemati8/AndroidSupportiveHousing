30d0e52cdca1240930313d8bb61448d8949ec25a
hanumasuseela <poomdlasuseela100@gmail.com>
2024-03-06 12:10:55 -0800

    Smart Wellness Commit Four

--- app/src/main/java/com/example/bluetoothapplication/helpers/NotificationHelper.kt
+++ app/src/main/java/com/example/bluetoothapplication/helpers/NotificationHelper.kt
@@ -3,19 +3,14 @@ package com.example.kotlinconversionsupportivehousing.helpers
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 
 object NotificationHelper {
-    fun createNotificationChannel(
-        context: Context,
-        notificationID: String?,
-        notificationName: String?
-    ) {
-        val channel = NotificationChannel(
-            notificationID,
-            notificationName,
-            NotificationManager.IMPORTANCE_HIGH
-        )
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotificationChannel(context: Context, notificationID: String?, notificationName: String?) {
+        val channel = NotificationChannel(notificationID, notificationName, NotificationManager.IMPORTANCE_HIGH)
         val notificationManager = context.getSystemService(
             NotificationManager::class.java
         )
