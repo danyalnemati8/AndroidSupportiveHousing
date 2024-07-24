31d9e90788316381ac0e47f4969c56f855bca77a
Danyal Nemati <Dnemati@csu.fullerton.edu>
2024-07-24 11:50:06 -0700

    smart pot ui update

--- app/src/main/java/com/example/bluetoothapplication/ui/smartpot/smartpotFragment.kt
+++ app/src/main/java/com/example/bluetoothapplication/ui/smartpot/smartpotFragment.kt
@@ -1,4 +1,13 @@
 package com.example.bluetoothapplication.ui.smartpot
 
 class smartpotFragment {
+
+    fun readWriteList() {
+        var mutableList = mutableListOf<String>()
+        mutableList.add("Sydney")
+        mutableList.add("Tokyo")
+        assert(mutableList.get(0) == "Sydney")
+        mutableList = mutableListOf("Paris", "London")
+        assert(mutableList.get(0) == "Paris")
+    }
 }
